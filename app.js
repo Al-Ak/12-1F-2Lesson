70a7b77b19cc1af926f1b84659ae6b303be073f2
Al-Ak <mutalibovsak@gmail.com>
2023-01-23 20:31:57 +0600

    2 lesson

---
+++ app.js
@@ -0,0 +1,26 @@
+var user = {
+    name:'Forever',
+    Network: 'American Broadcasting Company',
+    releaseDate:'September 22, 2014 (USA)',
+    mainactor:'Dr Henry Morgan - Real name: Ioan Gruffud',
+    episode: {
+        allepisode:'28',
+        Finalepisodedate: 'May 5, 2015',
+    },
+    Genres: 'Science fiction, Fantasy, Drama, Crime film, Medical drama',
+    Author: 'Matt Miller'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
